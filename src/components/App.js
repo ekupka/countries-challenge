8e0d6f3696e987f1b91d068908a60f33378bdc66
Eduardo Kupka <dudu.kupka@gmail.com>
2021-08-17 08:39:55 -0300

    Router and Apollo Provider inside App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,34 @@
 import './../styles/App.css';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Country from './Country';
 import Header from './Header';
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://testefront.dev.softplan.com.br/'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
 
 function App() {
 
   return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/:_id" component={Country} />
-      </Switch>
-    </div>
+    <Router>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/:_id" component={Country} />
+          </Switch>
+        </div>
+      </ApolloProvider>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,25 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@fontsource/roboto';
 import './styles/index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
-import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client';
-
-const httpLink = createHttpLink({
-  uri: 'http://testefront.dev.softplan.com.br/'
-});
-
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
-});
 
 ReactDOM.render(
-  <Router>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </ Router>,
-
-  document.getElementById('root')
+    <App />, 
+    document.getElementById('root')
 );
